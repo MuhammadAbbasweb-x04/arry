31b5e87a49033892c95ca650b220c496c919322b
MuhammadAbbas-web04 <mabbasmshahi2003@gmail.com>
2025-08-10 12:18:51 +0500

    added

---
+++ app.js
@@ -0,0 +1,45 @@
+// Q1
+// Answer
+// let studentnames = ["abbas","abuzar","ilyas"];
+// studentnames.push("muneeb");
+
+// Q2
+// Answer
+// let fruits = ["apple","mango","orange","kiwi"];
+// fruits.pop("kiwi");
+
+// Q3
+// Answer
+// let days = ["Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"];
+// days.unshift("Monday");
+
+// Q4
+// Answer
+// let colors = ["red","blue","green","pink","purple"];
+// colors.shift("red"); 
+
+// Q5
+// Answer
+// let sub = ["Engilsh","Urdu","Biology","Chemistry","Physics"];
+// sub.splice(2, 2, "Math", "Science");
+
+// console.log(sub);
+
+// Q6
+// Answer
+// let cities = ["karachi","quetta","sukkur","islamabad","khuzdar"];
+// cities.splice(1 ,1);
+// console.log(cities);
+
+// Q7
+// Answer
+// let countries = ["pakistan","turky","iran","poland","malta"];
+// let countrie= countries.slice(0, 2);
+// console.log(countries);
+
+
+// Q8
+// Answer
+l
+
+
